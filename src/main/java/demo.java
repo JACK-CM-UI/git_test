1ce1534000e8bc0271022dd2ee324736eb72fd11
zyh <1479863360@qq.com>
2024-09-14 15:06:52 +0800

    my third idea git_commit

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -2,5 +2,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("中秋快乐~");
         System.out.println("中秋快乐~");
+        System.out.println("中秋快乐~");
     }
 }
